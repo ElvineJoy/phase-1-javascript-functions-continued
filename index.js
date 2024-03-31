f2bfd132a47ca73773d685fde44a2da3004a5687
ElvineJoy <elvinkuiyungest@gmail.com>
2024-03-31 12:22:05 +0300

    first two functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    console.log('This Saturday, I want to' + " " + activity);
+    return 'This Saturday, I want to' + " " + activity + "!"
+}
+saturdayFun('bathe my dog')
+
+const mondayWork = function (workActivity = 'go to the office'){
+    console.log(`This Monday, I will ${workActivity}.`)    
+    return `This Monday, I will ${workActivity}.`
+}
+mondayWork('work from home')
+
